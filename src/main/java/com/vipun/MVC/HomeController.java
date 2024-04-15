d0d6c4e13bfc9a33a01d2e903af0522d5ff2ddf4
Vipun Sanjana Jayathunga <vipunsanjana34@gmail.com>
2024-04-15 23:20:32 +0530

    session part springboot MVC

--- src/main/java/com/vipun/MVC/HomeController.java
+++ src/main/java/com/vipun/MVC/HomeController.java
@@ -1,5 +1,7 @@
 package com.vipun.MVC;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +15,15 @@ public class HomeController {
     }
 
     @RequestMapping("add")
-    public String add(){
+    public String add(HttpServletRequest request){
+
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+
+        int result = num1 + num2;
+
+        HttpSession session = request.getSession();
+        session.setAttribute("result",result);
 
         return "result.jsp";
     }
--- src/main/webapp/result.jsp
+++ src/main/webapp/result.jsp
@@ -1,12 +1,15 @@
+<%@ page import="jakarta.servlet.http.HttpSession" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
+
+<% HttpSession session1 = request.getSession(); %>
 <head>
 
     <title>Hello Vipun JSP</title>
 </head>
 <body>
-    <h1>Result : </h1>
+    <h1>Result : <%= session1.getAttribute("result") %> </h1>
 
 </body>
 </html>
